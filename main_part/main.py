ce4bf15247a46646b985ace6e414a19b165cf71d
vito <zidzivik@cvut.cz>
2025-09-08 17:32:50 +0200

    main: Corrections in skeleton for main loop

--- main_part/main.py
+++ main_part/main.py
@@ -5,24 +5,24 @@ from machine import Pin
 
 
 #  _________This is used fro lcd display DO NOT REMOVE_________
-# ---------- Konfigurácia I2C ----------
-# Použijeme I2C(0) so SDA=GP0, SCL=GP1
-i2c = machine.I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
+# # ---------- Konfigurácia I2C ----------
+# # Použijeme I2C(0) so SDA=GP0, SCL=GP1
+# i2c = machine.I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
 
-# sken adres
-print("I2C scan:", i2c.scan())
+# # sken adres
+# print("I2C scan:", i2c.scan())
 
-# Správna adresa bežne 0x3C alebo 0x3D. Ak scan vráti [], skontroluj zapojenie.
-addrs = i2c.scan()
-if not addrs:
-    # let this print statement here
-    # will be used as log message 
-    print("OLED alebo I2C sa nenašlo. Skontroluj zapojenie a napájanie (3.3V).")
-else:
-    addr = addrs[0]
-    print("Použijem adresu 0x{:02X}".format(addr))
-    # vytvor SSD1306 inštanciu (128x64)
-    oled = SSD1306_I2C(128, 64, i2c, addr=addr)
+# # Správna adresa bežne 0x3C alebo 0x3D. Ak scan vráti [], skontroluj zapojenie.
+# addrs = i2c.scan()
+# if not addrs:
+#     # let this print statement here
+#     # will be used as log message 
+#     print("OLED alebo I2C sa nenašlo. Skontroluj zapojenie a napájanie (3.3V).")
+# else:
+#     addr = addrs[0]
+#     print("Použijem adresu 0x{:02X}".format(addr))
+#     # vytvor SSD1306 inštanciu (128x64)
+#     oled = SSD1306_I2C(128, 64, i2c, addr=addr)
 
     # # vyčisti displej
     # oled.fill(0)
@@ -48,9 +48,9 @@ else:
 
 
 # Define buttons (assuming pulled-up, so pressed = LOW)
-btn_water = Pin(14, Pin.IN, Pin.PULL_UP)   # button 1 – manual watering
-btn_display = Pin(15, Pin.IN, Pin.PULL_UP) # button 2 – display + lights
-btn_mode = Pin(16, Pin.IN, Pin.PULL_UP)    # button 3 – change light mode
+btn_water = Pin(0, Pin.IN, Pin.PULL_UP)   # button 1 – manual watering
+btn_display = Pin(1, Pin.IN, Pin.PULL_UP) # button 2 – display + lights
+btn_mode = Pin(2, Pin.IN, Pin.PULL_UP)    # button 3 – change light mode
 
 # States
 display_on = False
