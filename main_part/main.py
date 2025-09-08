d2a8c5d77c2a15ecfb84782ec7955d36bc457f0a
vito <zidzivik@cvut.cz>
2025-09-08 20:22:53 +0200

    main: Add time configuration to enable timestamps in logs and data.txt. Custom localtime() funtion needs to be created to adjust time based on timezone

--- main_part/main.py
+++ main_part/main.py
@@ -1,7 +1,47 @@
 from display_init import SSD1306_I2C
 import machine
 import time
+import ntptime
+import network
 from machine import Pin
+
+
+# ---------------------------------------
+# TIME CONFIGURATION
+# ---------------------------------------
+    # If program fails to connect to network, or NTP can't
+    # be accesed, program continues with warning and log messages
+    # and other data do not have timestamp. ( controlled with DATE_IS_SET 
+    # variable )
+
+DATE_IS_SET = False
+
+# Connect to Wi-Fi
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect("name", "password")
+
+# Add timeout to make it more robust
+timeout = 15
+while not wlan.isconnected() and timeout > 0:
+    time.sleep(1)
+    timeout -= 1
+
+if wlan.isconnected():
+    try:
+        ntptime.settime()
+        DATE_IS_SET = True
+    except:
+        # create log message
+        print("Could not get time from NTP")
+else:
+    # create log message
+    print("Wi-Fi not connected, using default time")
+
+# !!!!! custom localtime() funtion has to be written because of timezones !!!!
+rtc = machine.RTC()
+
+
 
 # ---------------------------------------
 # BUTTONS CONFIGURATION
@@ -41,6 +81,9 @@ else:
 
 
 
+# ---------------------------------------
+# HELPER FUNCTIONS
+# ---------------------------------------
 
 
 
@@ -48,34 +91,57 @@ else:
 
 
 
+# ---------------------------------------
+# MAIN LOOP
+# ---------------------------------------
+
+
+# States
+display_on = False
+lights_on = False
+party_mode = False
+
+while True:
 
 
 
 
 
+    # Button 1: manual watering
+    if not BTN_WATER.value():   # pressed
+        print("watering")
+        time.sleep(0.3)  # debounce
+
+    # Button 2: toggle display + lights
+    if not BTN_DISPLAY.value():   # pressed
+        display_on = not display_on
+        lights_on = display_on
+        if display_on:
+            print("display is on")
+            print("lights are turned on")
+        else:
+            print("display is off")
+            print("lights are turned off")
+            # clear the display
+            oled.fill(0)
+            oled.show()
+        time.sleep(0.3)  # debounce
+
+    # Button 3: change light mode
+    if not BTN_MODE.value() and lights_on:   # pressed
+        party_mode = not party_mode
+        if party_mode:
+            print("party mode")
+        else:
+            print("normal mode")
+        time.sleep(0.3)  # debounce
+
+    time.sleep(0.05)  # main loop delay
 
 
 
 
-#  _________This is used fro lcd display DO NOT REMOVE_________
-# # ---------- Konfigurácia I2C ----------
-# # Použijeme I2C(0) so SDA=GP0, SCL=GP1
-# i2c = machine.I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
 
-# # sken adres
-# print("I2C scan:", i2c.scan())
-
-# # Správna adresa bežne 0x3C alebo 0x3D. Ak scan vráti [], skontroluj zapojenie.
-# addrs = i2c.scan()
-# if not addrs:
-#     # let this print statement here
-#     # will be used as log message 
-#     print("OLED alebo I2C sa nenašlo. Skontroluj zapojenie a napájanie (3.3V).")
-# else:
-#     addr = addrs[0]
-#     print("Použijem adresu 0x{:02X}".format(addr))
-#     # vytvor SSD1306 inštanciu (128x64)
-#     oled = SSD1306_I2C(128, 64, i2c, addr=addr)
 
     # # vyčisti displej
     # oled.fill(0)
@@ -98,47 +164,5 @@ else:
     # oled.text("Spusteny main.py", 0, 28)
     # oled.show()
 # _______________________________________________________________
-
-
-
-
-# States
-display_on = False
-lights_on = False
-party_mode = False
-
-while True:
-    # Button 1: manual watering
-    if not BTN_WATER.value():   # pressed
-        print("watering")
-        time.sleep(0.3)  # debounce
-
-    # Button 2: toggle display + lights
-    if not BTN_DISPLAY.value():   # pressed
-        display_on = not display_on
-        lights_on = display_on
-        if display_on:
-            print("display is on")
-            print("lights are turned on")
-        else:
-            print("display is off")
-            print("lights are turned off")
-        time.sleep(0.3)  # debounce
-
-    # Button 3: change light mode
-    if not BTN_MODE.value() and lights_on:   # pressed
-        party_mode = not party_mode
-        if party_mode:
-            print("party mode")
-        else:
-            print("normal mode")
-        time.sleep(0.3)  # debounce
-
-    time.sleep(0.05)  # main loop delay
-
-
-
-
-
 
 
